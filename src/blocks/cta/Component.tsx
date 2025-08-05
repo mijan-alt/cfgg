eaa0e0c26760670a7bd73204cbe09059d3a28434
mijan-alt <mijanigoni@gmail.com>
2025-08-05 18:43:43 +0100

    fixed the background image

--- src/blocks/cta/Component.tsx
+++ src/blocks/cta/Component.tsx
@@ -1,9 +1,9 @@
+'use client'
+
 import React from 'react'
 import Link from 'next/link'
-
-import { Media } from '@/components/Media'
 import { Button } from '@/components/ui/button'
-import { Cta2 } from '@/payload-types'
+import { Cta2, Media } from '@/payload-types'
 
 export const CTA2Block: React.FC<Cta2> = ({
   heading,
@@ -22,18 +22,22 @@ export const CTA2Block: React.FC<Cta2> = ({
       ? `/${secondaryButton.link.slug}`
       : '#'
 
+
+      const bg = backgroundImage as Media
+
   return (
     <section className="">
       <div className="container">
-        <div className="relative h-[620px] overflow-hidden rounded-2xl bg-cover bg-center flex items-center justify-center">
-          <Media
-            resource={backgroundImage}
-            className="absolute inset-0 object-cover w-full h-full z-0"
-            imgClassName="h-full w-full"
-          />
-          <div className="absolute inset-0 bg-black bg-opacity-40 z-10" />
+        <div
+          className="relative h-[620px] overflow-hidden rounded-2xl flex items-center justify-center bg-fixed bg-cover bg-center"
+          style={{
+            backgroundImage: `url(${bg?.url})`,
+          }}
+        >
+          <div className="absolute inset-0 bg-black/50 z-10" />
+
           <div className="relative z-20 flex flex-col gap-8 p-4 text-center text-white max-w-2xl mx-auto">
-            <h2 className="text-5xl font-bold">{heading}</h2>
+            <h2 className="text-4xl md:text-5xl font-bold">{heading}</h2>
             <p className="text-lg">{subheading}</p>
             <div className="flex flex-col sm:flex-row justify-center gap-2">
               <Link href={primaryHref}>
