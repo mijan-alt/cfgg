637a3a0b42f621a295e0289d0614dc56c22e7a77
mijan-alt <mijanigoni@gmail.com>
2025-08-10 22:40:38 +0100

    changed the route for blogs

--- src/app/(frontend)/blogs/page.tsx
+++ src/app/(frontend)/blogs/page.tsx
@@ -1,12 +1,13 @@
 import type { Metadata } from 'next/types'
 
-import { NewsArchive } from '@/components/NewsArchive'
+
 import { PageRange } from '@/components/PageRange'
 import { Pagination } from '@/components/Pagination'
 import configPromise from '@payload-config'
 import { getPayload } from 'payload'
 import React from 'react'
 import PageClient from './page.client'
+import { BlogArchive } from '@/components/BlogsArchive'
 
 export const dynamic = 'force-static'
 export const revalidate = 600
@@ -48,7 +49,7 @@ export default async function Page() {
       </div>
 
       <div className=" mx-auto px-4 max-w-7xl">
-        <NewsArchive posts={posts.docs} />
+        <BlogArchive posts={posts.docs} />
       </div>
 
       <div className="container">
