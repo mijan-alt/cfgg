1e14889d67b96c48543baf6394ff4ffff6a2d433
mijan-alt <mijanigoni@gmail.com>
2025-08-11 22:12:14 +0100

    aligned the hero text to the left

--- src/heros/HighImpact/index.tsx
+++ src/heros/HighImpact/index.tsx
@@ -40,18 +40,18 @@ export const HighImpactHero: React.FC<Page['hero']> = ({ links, media, richText 
       <div className="absolute inset-0 bg-black/70 z-0"></div>
 
       {/* Hero content */}
-      <div className="container mb-8 z-10 relative flex items-center justify-center">
-        <div className="max-w-[36.5rem] md:text-center">
-          {richText && <RichText className="mb-6 " data={richText} enableGutter={false} />}
+      <div className="container mb-8 z-10 relative">
+        <div className="max-w-[36.5rem]">
+          {richText && <RichText className=" " data={richText} enableGutter={false} />}
           {Array.isArray(links) && links.length > 0 && (
-            <ul className="flex md:justify-center gap-4">
+            <ul className="flex gap-4">
               {links.map(({ link }, i) => (
                 <li key={i}>
                   <CMSLink {...link} />
                 </li>
               ))}
             </ul>
-          )}
+          )} 
         </div>
       </div>
     </div>
